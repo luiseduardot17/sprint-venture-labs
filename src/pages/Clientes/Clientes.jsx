b9c8acc73759d01f30b4ddd54070d3356ba9e4c4
luiseduardot17 <luis_eduardogdeo@hotmail.com>
2022-08-29 19:08:14 -0300

    style: estilização de Clientes.jsx

--- src/pages/Clientes/Clientes.jsx
+++ src/pages/Clientes/Clientes.jsx
@@ -1,11 +1,32 @@
-import React from 'react'
+import React from "react";
+import S from "./Clientes.module.css";
 
-const Clientes = () => {
+const Clientes = ({}) => {
   return (
-    <div>
-      
-    </div>
-  )
-}
+    <div className={S.containerClientes}>
+      <div className={S.container}>
+        <h2>Conheça nossos clientes</h2>
 
-export default Clientes
+        {/* {clientes.map((item, index)=> {
+          return(
+            <BancoDeDados
+            key={index}
+            nome={item.nome}
+            sobrenome={item.sobrenome}
+            email={item.email}
+            telefone={item.telefone}
+            cep={item.cep}
+            endereço1={item.endereço1}
+            endereço2={item.endereço2}
+            nascimento={item.nascimento}
+            cpf={item.cpf}
+            renda={item.renda}/>
+          )
+          
+          })} */}
+      </div>
+    </div>
+  );
+};
+
+export default Clientes;
